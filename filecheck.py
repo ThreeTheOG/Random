636db254570bc0de3d3740cc771be00fe579ab0f
Three <threetheog@gmail.com>
2021-09-06 22:10:58 -0700

    Forgot to save comments

--- filecheck.py
+++ filecheck.py
@@ -1,10 +1,10 @@
 import os
 
-# Grabs all files
+# Grabs all files in the Current Working Directory.
 for filename in os.listdir():
     
-    
+    # Checks which files end with '.py'.
     if filename.endswith('.py'):
         
-        
+        # Prints the filenames that end with '.py', and strips the last 3 characters.
         print(filename[:-3])
